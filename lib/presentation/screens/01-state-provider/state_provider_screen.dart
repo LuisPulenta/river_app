50d8a90765dfa5342454f30957206725bd75673b
LuisPulenta <luisalbertonu@gmail.com>
2023-06-29 19:13:11 -0300

    Video 12

--- lib/presentation/screens/01-state-provider/state_provider_screen.dart
+++ lib/presentation/screens/01-state-provider/state_provider_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_app/presentation/providers/providers.dart';
 
-class StateProviderScreen extends StatelessWidget {
+class StateProviderScreen extends ConsumerWidget {
   const StateProviderScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final titleStyle = Theme.of(context).textTheme.titleMedium;
+
+    final name = ref.watch(randomNameProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -14,7 +18,7 @@ class StateProviderScreen extends StatelessWidget {
       ),
       body: Center(
         child: Text(
-          'Luis',
+          name,
           style: titleStyle,
         ),
       ),
